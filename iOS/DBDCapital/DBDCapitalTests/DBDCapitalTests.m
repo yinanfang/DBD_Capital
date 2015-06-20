703fcbe7e6ba35a40db85ba2d0c583f2cef84a61
Lucas Fang <yinan_fang@hotmail.com>
2015-06-20 09:28:07 -0700

    Add iOS starter project

---
+++ iOS/DBDCapital/DBDCapitalTests/DBDCapitalTests.m
@@ -0,0 +1,40 @@
+//
+//  DBDCapitalTests.m
+//  DBDCapitalTests
+//
+//  Created by Lucas Fang on 6/20/15.
+//  Copyright (c) 2015 DBD Capital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface DBDCapitalTests : XCTestCase
+
+@end
+
+@implementation DBDCapitalTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
