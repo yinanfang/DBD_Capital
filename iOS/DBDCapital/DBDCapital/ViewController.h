f4578fbba4afb1cd6f8e5c98b8549cb5c1dbe6fe
Lucas Fang <yinan_fang@hotmail.com>
2015-06-28 11:51:42 -0700

    Re-add the iOS project as a universal project

--- iOS/DBDCapital/DBDCapital/ViewController.h
+++ iOS/DBDCapital/DBDCapital/ViewController.h
@@ -2,7 +2,7 @@
 //  ViewController.h
 //  DBDCapital
 //
-//  Created by Lucas Fang on 6/20/15.
+//  Created by Lucas Fang on 6/28/15.
 //  Copyright (c) 2015 DBD Capital. All rights reserved.
 //
 
